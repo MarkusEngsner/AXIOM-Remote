2037c4e7fa1450e30967b4d11f3f09bad4d10e21
Markus Engsner <markus.engsner@gmail.com>
2021-03-17 22:23:56 +0100

    Remove commented out code

--- Firmware/UI/Screens/MainPage.cpp
+++ Firmware/UI/Screens/MainPage.cpp
@@ -62,15 +62,9 @@ void MainPage::Draw(IPainter* painter)
 {
     painter->DrawIcon(&ApertusTextLogo, 58, 89, RGB565(194, 191, 188));
     painter->DrawIcon(&ApertusRingLogo, 58 + ApertusTextLogo.Width, 89, RGB565(244, 114, 72));
-    // std::array<uint16_t, 4> grayScalePalette{static_cast<uint16_t>(Color565::Black),
-    //                                          static_cast<uint16_t>(Color565::DarkGrey),
-    //                                          static_cast<uint16_t>(Color565::LightGrey), 0};
     uint16_t grayScalePalette[4]{static_cast<uint16_t>(Color565::Black), static_cast<uint16_t>(Color565::DarkGrey),
                                  static_cast<uint16_t>(Color565::LightGrey), 0};
     // std::array<uint16_t, 4> cyanPalette{static_cast<uint16_t>(Color565::Navy),
-    //                                     static_cast<uint16_t>(Color565::DarkCyan),
-    //                                     static_cast<uint16_t>(Color565::Cyan),
-    //                                     static_cast<uint16_t>(Color565::White)};
     uint16_t cyanPalette[4]{static_cast<uint16_t>(Color565::Navy), static_cast<uint16_t>(Color565::DarkCyan),
                             static_cast<uint16_t>(Color565::Cyan), static_cast<uint16_t>(Color565::White)};
 
